b7cb5236383cc2e4087ec2c2e59abcfd8195e783
m7mdra <xm7mdrax@gmail.com>
2021-02-14 18:05:12 +0200

    Added libs and image

--- app/src/main/java/com/example/composeexample/MainActivity.kt
+++ app/src/main/java/com/example/composeexample/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.composeexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.tooling.preview.Preview
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent() {
+            OnBoarding()
+        }
+    }
+
+    @Composable
+    @Preview
+    fun OnBoarding() {
+        Surface(color = Color.Magenta,content = {
+
+        })
 
     }
 }
